1e19618b29965502f25e41261428de2f431d0ba1
Ayush-kr-giga <kumarayushsharma2@gmail.com>
2025-05-24 13:54:34 +0530

    added functionality to play button

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import './App.css'
 import Home from './components/Home'
 import Rules from './components/Rules'
+import Play from './components/Play';
 
 
 function App() {
 
   const [showRules,changeShowRules]=useState(false);
+  const [isPlaying,setIsPlaying]=useState(false);
 
   return (
     <>
-     {!showRules&& <Home changeShowRules={changeShowRules} />}
+     {!isPlaying && !showRules&& <Home changeShowRules={changeShowRules} setIsPlaying={setIsPlaying}/>}
      {showRules&&<Rules  changeShowRules={changeShowRules}/>}
+     {isPlaying&&<Play/>}
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-const Home = ({changeShowRules}) => {
+const Home = ({changeShowRules,setIsPlaying}) => {
     return (
       <div className="flex flex-col items-center justify-center mx-auto my-[10vh] p-6 w-full h-[50vh]">
         <h1 className="text-4xl sm:text-5xl lg:text-6xl text-[#48D2FE] font-bold font-mono mb-8 text-center">
@@ -6,7 +6,11 @@ const Home = ({changeShowRules}) => {
         </h1>
         
         <div className="flex gap-6">
-          <button className="bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-6 rounded-xl transition">
+          <button className="bg-yellow-400 hover:bg-yellow-300 text-black font-bold py-2 px-6 rounded-xl transition"
+            onClick={()=>{
+            setIsPlaying((prev)=>!prev)
+          }}
+          >
             Play
           </button>
           <button className="bg-[#48D2FE] hover:bg-[#3bbde5] text-black font-bold py-2 px-6 rounded-xl transition"
